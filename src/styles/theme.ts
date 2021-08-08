ea10666691302d131daabaa895a6ccde3243174f
willian-lemann <willianleman@hotmail.com>
2021-08-07 22:03:09 -0300

    init project

---
+++ src/styles/theme.ts
@@ -0,0 +1,13 @@
+const theme = {
+  colors: {
+    primary: "#8257e6",
+    secondary: "#000",
+  },
+
+  fonts: {
+    primary: "roboto",
+    secondary: "inter",
+  },
+};
+
+export default theme;
