22cf3f523eed3200e4576264cbe2932ea634020e
willian-lemann <willianleman@hotmail.com>
2021-08-11 01:11:50 -0300

    feature: adding test provider wrapper

---
+++ src/__test__/config.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from 'react';
+import { ThemeProvider } from 'styled-components/native';
+import theme from 'styles/theme';
+
+interface TestThemeProviderProps {
+  children: ReactNode;
+}
+
+export const TestThemeProvider = ({ children }: TestThemeProviderProps) => (
+  <ThemeProvider theme={theme}>{children}</ThemeProvider>
+);
