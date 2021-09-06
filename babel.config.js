76f5131e0ff219cf472715031a754bd6b7648f0c
willian-lemann <willianleman@hotmail.com>
2021-09-06 20:26:16 -0300

    refactor: babel config paths setup update

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,25 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['react-native-reanimated/plugin']
+    plugins: [
+      'react-native-reanimated/plugin',
+      [
+        'module-resolver',
+        {
+          root: ['.'],
+          extensions: ['.js', '.jsx', '.es', '.es6', '.mjs', '.ts', '.tsx'],
+          alias: {
+            config: './src/config',
+            components: './src/components',
+            screens: './src/screens',
+            hooks: './src/hooks',
+            store: './src/store',
+            styles: './src/styles',
+            icons: './src/assets/icons',
+          },
+        },
+      ],
+    ],
   };
 };
